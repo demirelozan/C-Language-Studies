25dda8709cf63ca1adf91be4ec947d0dd914c21d
Okan Demirel <57192515+demirelozan@users.noreply.github.com>
2021-05-26 00:49:06 +0300

    Create patients.h

---
+++ PatientsData/patients.h
@@ -0,0 +1,24 @@
+#ifndef CUSTOMER_H
+#define CUSTOMER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct PatientStruct Patient;
+struct PatientStruct
+{
+	int id;
+	char name[50];
+	char surname[50];
+	char birthday[50];
+	char email[100];
+	char department[50];
+	//int orders[10];
+};
+
+Patient createPatient(char* name, char* surname, char* birthday, char* email, char* department);
+Patient* createPatientDynamic(char* name, char* surname, char* birthday,char* email, char* department);
+void stringfyPatient(Patient patient, char* buffer);
+
+#endif
