5cbea5a9c4b95d8c0316374e517c42146c549b4f
Okan Demirel <57192515+demirelozan@users.noreply.github.com>
2021-05-28 12:44:47 +0300

    Patients Header

--- PatientsData/patients.h
+++ PatientsData/patients.h
@@ -1,5 +1,5 @@
-#ifndef PATIENTS_H
-#define PATIENTS_H
+#ifndef CUSTOMER_H
+#define CUSTOMER_H
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,15 +9,16 @@ typedef struct PatientStruct Patient;
 struct PatientStruct
 {
 	int id;
-	int birthday;
 	char name[50];
-	char email[50];
+	char surname[50];
+	char birthday[50];
+	char email[100];
 	char department[50];
 	//int orders[10];
 };
 
-Patient createPatients(int birthday, char* name, char* email, char* department);
-Patient* createPatientDynamic(int birthday, char* name, char* email, char* department);
+Patient createPatient(char* name, char* surname, char* birthday, char* email, char* department);
+Patient* createPatientDynamic(char* name, char* surname, char* birthday,char* email, char* department);
 void stringfyPatient(Patient patient, char* buffer);
 
 #endif
