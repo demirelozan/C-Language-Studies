7f6fa522b53c3e9a5bd39e0b6554722806e26324
Okan Demirel <57192515+demirelozan@users.noreply.github.com>
2021-05-28 12:42:09 +0300

    Update patients.c

--- PatientsData/patients.c
+++ PatientsData/patients.c
@@ -1,28 +1,26 @@
 #include "patients.h"
 
-Patient createPatient(char* name, char* surname, char* birthday, char* email, char* department)
+Patient createPatient(int birthday, char* name, char* email, char* department)
 {
 	Patient patient;
 	patient.id = 0;
+	patient.birthday = birthday;
 	strcpy(patient.name, name);
-	strcpy(patient.surname, surname);
-	strcpy(patient.birthday, birthday);
 	strcpy(patient.email, email);
 	strcpy(patient.department, department);
 	return patient;
 }
-Patient* createPatientDynamic(char* name, char* surname, char* birthday, char* email, char* department)
+Patient* createPatientDynamic(int birthday, char* name, char* email, char* department)
 {
 	Patient* patient = (Patient*) malloc(sizeof(Patient));
 	patient->id = 0;
+	patient->birthday = birthday;
 	strcpy(patient->name, name);
-	strcpy(patient->surname, surname);
-	strcpy(patient->birthday, birthday);
 	strcpy(patient->email, email);
 	strcpy(patient->department, department);
 	return patient;
 }
 void stringfyPatient(Patient patient, char* buffer)
 {
-	sprintf(buffer,"%d %s %s %s %s %s",patient.id, patient.name, patient.surname, patient.birthday ,patient.email, patient.department);
+	sprintf(buffer,"%d %d %s %s %s",patient.id, patient.birthday, patient.name, patient.email, patient.department);
 }
